6b8f2d31da196098d3a1591991a1a3fff6b81c4d
bouslougui <infobachir@gmail.com>
2021-06-04 13:03:24 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+
+from werkzeug.utils import secure_filename
+import numpy as np
+
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+IMAGE_SIZE = (150, 150)
+UPLOAD_FOLDER = 'uploads'
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+
+
+@app.route("/")
+def template_test():
+    return render_template('home.html', label='', imagesource='file://null')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            #output = predict(file_path)
+    return render_template("home.html", label=output, imagesource=file_path)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == "__main__":
+    app.run(threaded=True,debug=True)
